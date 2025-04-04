8058a26f9bcab6b3a8412f3d50522cef1ca2dd81
ChrisGbr <chris17.grabener@gmail.com>
2025-04-04 00:58:13 +0000

    Konflikt gelöst + neue Skripte für 2-Phasen-Rollout

---
+++ terraform/ad_setup.tf
@@ -0,0 +1,22 @@
+resource "azurerm_virtual_machine_extension" "ad_setup" {
+  name                 = "setup-ad"
+  virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File setup-domain.ps1"
+    }
+SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+      "fileUris": [
+        "https://raw.githubusercontent.com/ChrisGbr/franchise-rollout/feature/terraform-extension-ad/scripts/setup-domain.ps1"
+      ]
+    }
+PROTECTED_SETTINGS
+}
+
