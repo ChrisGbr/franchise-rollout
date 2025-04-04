46f07d72f32e77ac95c52ed684e17e6902c23d4d
ChrisGbr <chris17.grabener@gmail.com>
2025-04-04 00:54:53 +0000

    main.tf split for 2 ps1 scripts

---
+++ terraform/main.tf
@@ -0,0 +1,73 @@
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-franchise-berlin01"
+  location = "Germany West Central"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-berlin01"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-berlin01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-berlin01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-berlin01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "vm-ad-berlin01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2ms"
+  admin_username      = "franchiseadmin"
+  admin_password      = "P@ssw0rd123!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+
+  provision_vm_agent = true
+  enable_automatic_updates = true
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.pip.ip_address
+}
