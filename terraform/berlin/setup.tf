85cac3c548ad89b70df181e83eb593c37c1cc782
ChrisGbr <chris17.grabener@gmail.com>
2025-04-08 23:48:58 +0000

    Dopplung Fix

--- terraform/berlin/setup.tf
+++ terraform/berlin/setup.tf
@@ -19,27 +19,3 @@ resource "azurerm_virtual_machine_extension" "bootstrap_ad_berlin" {
   }
   PROTECTED_SETTINGS
 }
-
-resource "azurerm_virtual_machine_extension" "configure_ou_berlin" {
-  name                 = "configure-ou-berlin"
-  virtual_machine_id   = azurerm_windows_virtual_machine.vm_berlin.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.10"
-
-  settings = <<SETTINGS
-  {
-    "commandToExecute": "powershell.exe -ExecutionPolicy Bypass -File configure-ou.ps1"
-  }
-  SETTINGS
-
-  protected_settings = <<PROTECTED_SETTINGS
-  {
-    "fileUris": [
-      "https://raw.githubusercontent.com/ChrisGbr/franchise-rollout/feature/terraform-extension-ad/scripts/configure-ou.ps1"
-    ]
-  }
-  PROTECTED_SETTINGS
-
-  depends_on = [azurerm_virtual_machine_extension.bootstrap_ad_berlin]
-}
