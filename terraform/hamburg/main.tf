d4351da02fc43a78bf96720f8451f4a760a9b5ac
ChrisGbr <chris17.grabener@gmail.com>
2025-04-08 20:52:46 +0000

    Add: Hamburg-Standort inkl. vollständiger AD & OU-Automatisierung

--- terraform/hamburg/main.tf
+++ terraform/hamburg/main.tf
@@ -2,72 +2,92 @@ provider "azurerm" {
   features {}
 }
 
-
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg_hamburg" {
   name     = "rg-franchise-hamburg01"
-  location = "Germany West Central"
+  location = "GermanyNorth"
 }
 
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "vnet_hamburg" {
   name                = "vnet-hamburg01"
-  address_space       = ["10.10.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.11.0.0/16"]
+  location            = azurerm_resource_group.rg_hamburg.location
+  resource_group_name = azurerm_resource_group.rg_hamburg.name
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "subnet_hamburg" {
   name                 = "subnet-hamburg01"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.1.0/24"]
+  resource_group_name  = azurerm_resource_group.rg_hamburg.name
+  virtual_network_name = azurerm_virtual_network.vnet_hamburg.name
+  address_prefixes     = ["10.11.0.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-hamburg01"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_network_security_group" "nsg_hamburg" {
+  name                = "nsg-hamburg01"
+  location            = azurerm_resource_group.rg_hamburg.location
+  resource_group_name = azurerm_resource_group.rg_hamburg.name
 
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_public_ip" "pip" {
+resource "azurerm_public_ip" "pip_hamburg" {
   name                = "pip-hamburg01"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg_hamburg.location
+  resource_group_name = azurerm_resource_group.rg_hamburg.name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 }
 
-resource "azurerm_windows_virtual_machine" "vm" {
+resource "azurerm_network_interface" "nic_hamburg" {
+  name                = "nic-hamburg01"
+  location            = azurerm_resource_group.rg_hamburg.location
+  resource_group_name = azurerm_resource_group.rg_hamburg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet_hamburg.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip_hamburg.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc_hamburg" {
+  network_interface_id      = azurerm_network_interface.nic_hamburg.id
+  network_security_group_id = azurerm_network_security_group.nsg_hamburg.id
+}
+
+resource "azurerm_windows_virtual_machine" "vm_hamburg" {
   name                = "vm-ad-hamburg01"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
+  location            = azurerm_resource_group.rg_hamburg.location
+  resource_group_name = azurerm_resource_group.rg_hamburg.name
+  size                = "Standard_D2s_v3"
   admin_username      = "franchiseadmin"
   admin_password      = "P@ssw0rd123!"
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
+  network_interface_ids = [azurerm_network_interface.nic_hamburg.id]
+  provision_vm_agent  = true
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2022-Datacenter"
     version   = "latest"
   }
-
-  provision_vm_agent = true
-  enable_automatic_updates = true
 }
 
 output "public_ip_address_hamburg" {
-  value = azurerm_public_ip.pip.ip_address
+  value = azurerm_public_ip.pip_hamburg.ip_address
 }
--- terraform/hamburg/setup.tf
+++ terraform/hamburg/setup.tf
@@ -1,22 +1,45 @@
-resource "azurerm_virtual_machine_extension" "ad_setup" {
-  name                 = "setup-ad-hamburg"
-  virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
+resource "azurerm_virtual_machine_extension" "bootstrap_ad_hamburg" {
+  name                 = "bootstrap-ad-hamburg"
+  virtual_machine_id   = azurerm_windows_virtual_machine.vm_hamburg.id
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.10"
 
   settings = <<SETTINGS
-    {
-      "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File setup-domain.ps1"
-    }
-SETTINGS
+  {
+    "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File setup-domain.ps1"
+  }
+  SETTINGS
 
   protected_settings = <<PROTECTED_SETTINGS
-    {
-      "fileUris": [
-        "https://raw.githubusercontent.com/ChrisGbr/franchise-rollout/feature/terraform-extension-ad/scripts/setup-domain.ps1",
-        "https://raw.githubusercontent.com/ChrisGbr/franchise-rollout/feature/terraform-extension-ad/scripts/configure-ou.ps1"
-      ]
-    }
-PROTECTED_SETTINGS
+  {
+    "fileUris": [
+      "https://raw.githubusercontent.com/ChrisGbr/franchise-rollout/feature/terraform-extension-ad/scripts/setup-domain.ps1"
+    ]
+  }
+  PROTECTED_SETTINGS
+}
+
+resource "azurerm_virtual_machine_extension" "configure_ou_hamburg" {
+  name                 = "configure-ou-hamburg"
+  virtual_machine_id   = azurerm_windows_virtual_machine.vm_hamburg.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  settings = <<SETTINGS
+  {
+    "commandToExecute": "powershell.exe -ExecutionPolicy Bypass -File configure-ou.ps1"
+  }
+  SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+  {
+    "fileUris": [
+      "https://raw.githubusercontent.com/ChrisGbr/franchise-rollout/feature/terraform-extension-ad/scripts/configure-ou.ps1"
+    ]
+  }
+  PROTECTED_SETTINGS
+
+  depends_on = [azurerm_virtual_machine_extension.bootstrap_ad_hamburg]
 }
